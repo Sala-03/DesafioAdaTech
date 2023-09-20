dc2e97b37836081ff9fe0273fb713e0e0a308b20
Robert-Martins <robertcantares@gmail.com>
2023-09-20 19:47:06 -0300

    feat/implement-api-documentation

--- Desafio1/src/main/java/com/desafio/um/infrastructure/configs/OpenAPIConfig.java
+++ Desafio1/src/main/java/com/desafio/um/infrastructure/configs/OpenAPIConfig.java
@@ -1,4 +1,34 @@
 package com.desafio.um.infrastructure.configs;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Desafio 1",
+                description = "API para gerenciamento de Feedbacks em Filas FIFO",
+                version = "1.0.0"
+        )
+)
 public class OpenAPIConfig {
+
+    @Bean
+    public GroupedOpenApi controllers() {
+        return GroupedOpenApi.builder()
+                .group("Endpoints")
+                .packagesToScan("com.desafio.um.presentation.controllers")
+                .build();
+    }
+
+    @Bean
+    public SwaggerUiConfigParameters swaggerUiConfigParameters() {
+        return new SwaggerUiConfigParameters(new SwaggerUiConfigProperties());
+    }
+
 }
