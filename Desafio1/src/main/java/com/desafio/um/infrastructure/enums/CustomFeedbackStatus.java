46b0d4b2959794158ec07e6eed714078c4bbd474
Felipe Santa Cruz Simão <felipesantacruz6@gmail.com>
2023-09-20 12:06:01 -0300

    feat(CustomFeedbackStatus) adding CustomFeedbackStatus enum

--- Desafio1/src/main/java/com/desafio/um/infrastructure/enums/CustomFeedbackStatus.java
+++ Desafio1/src/main/java/com/desafio/um/infrastructure/enums/CustomFeedbackStatus.java
@@ -1,4 +1,12 @@
 package com.desafio.um.infrastructure.enums;
 
 public enum CustomFeedbackStatus {
+    RECEBIDO("recebido"),
+    EMPROCESSAMENTO("emprocessamento"),
+    FINALIZADO("finalizado");
+
+    private String description;
+    CustomFeedbackStatus(String description) {
+        this.description = description;
+    }
 }
