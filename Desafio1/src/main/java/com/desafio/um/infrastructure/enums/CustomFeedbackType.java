0ec6230c6db1689368ceb71bea31bb30c23223e1
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2023-09-19 21:46:23 -0300

    feat(Custom Feedback):
    - Did set enum values.
    
    - Did create a contructor to get enum values.

--- Desafio1/src/main/java/com/desafio/um/infrastructure/enums/CustomFeedbackType.java
+++ Desafio1/src/main/java/com/desafio/um/infrastructure/enums/CustomFeedbackType.java
@@ -1,4 +1,11 @@
 package com.desafio.um.infrastructure.enums;
-
 public enum CustomFeedbackType {
+    SUSGESTOES("sugestoes"),
+    ELOGIOS("elogios"),
+    CRITICAS("criticas");
+
+    private String description;
+    CustomFeedbackType(String description) {
+        this.description = description;
+    }
 }
