7509525e19a946741b11b5bd6ab42563c2133a65
Ana Paula <anafujarra@yahoo.com.br>
2023-09-21 19:46:34 -0300

    Review Class Service and InterfaceService

--- Desafio1/src/main/java/com/desafio/um/presentation/controllers/CustomerFeedbackController.java
+++ Desafio1/src/main/java/com/desafio/um/presentation/controllers/CustomerFeedbackController.java
@@ -1,12 +1,12 @@
 package com.desafio.um.presentation.controllers;
 
+import com.desafio.um.domain.services.CustomerFeedbackService;
 import com.desafio.um.presentation.services.ICustomerFeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.springbootstarter.service.QueueService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/queue")
+@RequestMapping("/")
 public class CustomerFeedbackController {
 
     @Autowired
@@ -14,7 +14,7 @@ public class CustomerFeedbackController {
 
     @PostMapping("/enqueue")
     public void enqueue(@RequestBody String item) {
-        queueService.enqueue(item);
+        CustomerFeedbackService.(item);
     }
 
     @GetMapping("/dequeue")
