a80d746307f6f801bb52af8971c25438dcb31d09
Robert-Martins <robertcantares@gmail.com>
2023-09-24 21:57:08 -0300

    feat/config-allow-origins

---
+++ Desafio1/src/main/java/com/desafio/um/infrastructure/configs/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.desafio.um.infrastructure.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig  extends CorsFilter {
+
+    public CorsConfig() {
+        super(configurationSource());
+    }
+
+    private static CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+}
